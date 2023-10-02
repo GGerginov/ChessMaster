44dc416ee928e29a81c3019b94a00b9d66093138
GGerginov <electro44@abv.bg>
2023-10-02 14:53:13 +0300

    Added JavaDoc

--- src/main/java/org/example/chess/ChessMaster.java
+++ src/main/java/org/example/chess/ChessMaster.java
@@ -5,6 +5,10 @@ import org.example.exception.InvalidPositionException;
 
 import java.util.*;
 
+/**
+ * The ChessMaster class represents a chess game master that controls the movement of a knight on a 3x3 chessboard.
+ * It provides methods to play the chess game and determine the number of moves required for the knight to reach a target position.
+ */
 public class ChessMaster {
 
     private static final Integer BOARD_SIZE = 3;
@@ -20,9 +24,19 @@ public class ChessMaster {
 
     private final Set<Position> visited = new HashSet<>();
 
+
+    /**
+     * Plays the chess game starting from the given startPosition and returns the number of moves required for the knight to reach the target position.
+     * If the target position is reached, the method returns the number of moves. If the target position cannot be reached, the method returns -1.
+     *
+     * @param startPosition The starting position of the knight on the chessboard.
+     * @return The number of moves required for the knight to reach the target position, or -1 if the target position cannot be reached.
+     * @throws InvalidPositionException If the start position is not within the 3x3 chessboard.
+     */
     public int playChess(Position startPosition) throws InvalidPositionException {
 
-        if (!isValidPosition(startPosition)) throw new InvalidPositionException("Start position is not within the 3x3 chess board.");
+        if (!isValidPosition(startPosition))
+            throw new InvalidPositionException("Start position is not within the 3x3 chess board.");
         positions.add(startPosition);
         visited.add(startPosition);
 
@@ -54,6 +68,12 @@ public class ChessMaster {
     }
 
 
+    /**
+     * Checks if the given position is a valid position on the chessboard.
+     *
+     * @param position The position to check.
+     * @return True if the position is valid, false otherwise.
+     */
     private boolean isValidPosition(Position position) {
         return position.getX() > 0 &&
                 position.getX() <= BOARD_SIZE &&
@@ -61,6 +81,12 @@ public class ChessMaster {
                 position.getY() <= BOARD_SIZE;
     }
 
+    /**
+    Checks if the given position is the target position.
+
+    @param position The position to check.
+    @return True if the position is the target position, false otherwise.
+    */
     private boolean isTargetReached(Position position) {
         return position.getX() == TARGET_X && position.getY() == TARGET_Y;
     }
--- src/main/java/org/example/domain/Position.java
+++ src/main/java/org/example/domain/Position.java
@@ -1,5 +1,9 @@
 package org.example.domain;
 
+/**
+ * The `Position` class represents a position on a two-dimensional chess board.
+ * It contains the x and y coordinates of the position.
+ */
 public class Position {
     private final int x;
     private final int y;
@@ -9,14 +13,28 @@ public class Position {
         this.y = builder.y;
     }
 
+    /**
+     * Returns the x coordinate of the position.
+     *
+     * @return the x coordinate
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Returns the y coordinate of the position.
+     *
+     * @return the y coordinate
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * The `Builder` class is used to construct a `Position` object.
+     * It provides methods to set the x and y coordinates.
+     */
     public static class Builder {
         private int x;
         private int y;
@@ -24,21 +42,45 @@ public class Position {
         public Builder() {
         }
 
+        /**
+         * Sets the x coordinate of the position.
+         *
+         * @param x the x coordinate
+         * @return the builder object
+         */
         public Builder x(int x) {
             this.x = x;
             return this;
         }
 
+        /**
+         * Sets the y coordinate of the position.
+         *
+         * @param y the y coordinate
+         * @return the builder object
+         */
         public Builder y(int y) {
             this.y = y;
             return this;
         }
 
+        /**
+         * Builds a new `Position` object with the specified coordinates.
+         *
+         * @return the new `Position` object
+         */
         public Position build() {
             return new Position(this);
         }
     }
 
+    /**
+     * Checks if this `Position` object is equal to another object.
+     * Two `Position` objects are considered equal if they have the same x and y coordinates.
+     *
+     * @param o the object to compare
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -48,6 +90,11 @@ public class Position {
         return getY() == position.getY();
     }
 
+    /**
+     * Returns the hash code value for this `Position` object.
+     *
+     * @return the hash code value
+     */
     @Override
     public int hashCode() {
         int result = getX();
